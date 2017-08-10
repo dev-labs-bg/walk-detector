580efe94a3f3bc3be14d9231f820f5d362fd2b94
Simona Stoyanova <sstoyanova26@gmail.com>
2017-08-10 12:03:30 +0300

    Added documentation Handled deprecated Notification.Builder

--- app/src/main/java/bg/devlabs/walkdetector/util/DateTimeHelper.java
+++ app/src/main/java/bg/devlabs/walkdetector/util/DateTimeHelper.java
@@ -5,30 +5,41 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * Created by simona on 8/9/17.
+ * Created by Simona Stoyanova on 8/9/17.
+ * simona@devlabs.bg
+ * <p>
+ * A helper class which validates the entered time values
+ * and helps with Calendar calculations for the every day alarm times
  */
 
 public class DateTimeHelper {
     // tag used for logging purposes
     private static final String TAG = DateTimeHelper.class.getSimpleName();
 
+    /**
+     * Validates the user input
+     * @param startTime user input time
+     * @return true if the format is correct and false otherwise
+     */
     public static boolean isTimeValid(String startTime) {
+        // Pattern that matches the 24 hour minutes time format
+        // For example 23:59 is a valid input
         Pattern p = Pattern.compile("^([0-1]\\d|2[0-3]):([0-5]\\d)$");
         Matcher m = p.matcher(startTime);
         return m.matches();
     }
 
-
+    /**
+     * Sets the hours and minutes to the calendar, so it can be used for everyday alarms
+     * @param startTime used to get the hour and minutes in the HH:mm format
+     * @return time in millis  of the calendar
+     */
     public static long getCalendarTimeMillis(String startTime) {
-
         Calendar calendar = Calendar.getInstance();
         calendar.set(Calendar.HOUR_OF_DAY, Integer.valueOf(startTime.substring(0, 2))); // For 1 PM or 2 PM
         calendar.set(Calendar.MINUTE, Integer.valueOf(startTime.substring(3, 5)));
         calendar.set(Calendar.SECOND, 0);
         calendar.set(Calendar.MILLISECOND, 0);
         return calendar.getTimeInMillis();
-
-
     }
-
 }
--- app/src/main/java/bg/devlabs/walkdetector/util/NotificationHelper.java
+++ app/src/main/java/bg/devlabs/walkdetector/util/NotificationHelper.java
@@ -8,12 +8,14 @@ import android.graphics.Color;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
-import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 
 import bg.devlabs.walkdetector.MainActivity;
 import bg.devlabs.walkdetector.R;
+
+import static android.app.NotificationChannel.DEFAULT_CHANNEL_ID;
 
 /**
  * Created by Simona Stoyanova on 8/9/17.
@@ -25,6 +27,7 @@ import bg.devlabs.walkdetector.R;
 public class NotificationHelper {
     // tag used for logging purposes
     private static final String TAG = NotificationHelper.class.getSimpleName();
+    private static final String APP_TO_OPEN_PACKAGE_NAME = "com.charitymilescm.android";
 
     /**
      * Posts a notification in the notification bar when a transition is detected.
@@ -34,27 +37,29 @@ public class NotificationHelper {
         playNotificationSound(context);
 
         Log.d(TAG, "sendNotification " + message);
-        // Create an explicit content Intent that starts the main Activity.
-        Intent notificationIntent = context.getPackageManager().getLaunchIntentForPackage("com.charitymilescm.android");
+        // Create an explicit content Intent that starts the APP_TO_OPEN_PACKAGE_NAME.
+        Intent notificationIntent = context.getPackageManager().getLaunchIntentForPackage(APP_TO_OPEN_PACKAGE_NAME);
+        // If the app is not installed on the device the Home screen is opened
         if (notificationIntent == null) {
             notificationIntent = new Intent(context, MainActivity.class);
         }
 
         notificationIntent.putExtra("notificationDetails", message);
-
         // Construct a task stack.
         TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
-
         // Push the content Intent onto the stack.
         stackBuilder.addNextIntent(notificationIntent);
-
         // Get a PendingIntent containing the entire back stack.
         PendingIntent notificationPendingIntent = stackBuilder.getPendingIntent(0,
                 PendingIntent.FLAG_UPDATE_CURRENT);
-
         // Get a notification builder that's compatible with platform versions >= 4
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
-
+        NotificationCompat.Builder builder = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            builder = new NotificationCompat.Builder(context, DEFAULT_CHANNEL_ID);
+        } else {
+            // It is deprecated but the definition above is not supported on older versions
+            builder = new NotificationCompat.Builder(context);
+        }
         // Define the notification settings.
         builder.setSmallIcon(R.drawable.ic_directions_walk_black_24dp)
                 .setColor(Color.RED)
@@ -76,6 +81,7 @@ public class NotificationHelper {
 
     /**
      * Plays the default notification sound
+     *
      * @param context needed to get the notification ringtone from RingtoneManager
      */
     private static void playNotificationSound(Context context) {
